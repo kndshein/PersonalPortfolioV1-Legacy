95d39c27984579cf003931e210785252e9799a7f
Kaung Shein <nicholasling1412@gmail.com>
2020-12-24 20:14:39 -0500

    Revamped Landing Page

--- js/app.js
+++ js/app.js
@@ -7,13 +7,13 @@ $.ajax(
   "https://spreadsheets.google.com/feeds/list/1ZYlV-gAMjH1KxIEfc24jhPFtLRe3hfE-3TcwIzMoh1o/1/public/full?alt=json"
 ).then((data) => {
   // Checking my data
-  // console.log(data);
+  console.log(data);
 
   // Put our projects in a variable
   const rawProjects = data.feed.entry;
 
   // Log our projects
-  // console.log(rawProjects);
+  console.log(rawProjects);
 
   // Prettify our projects array
   const projects = rawProjects.map((project) => {
@@ -21,8 +21,7 @@ $.ajax(
       name: project.gsx$name.$t,
       image: project.gsx$image.$t,
       description: project.gsx$description.$t,
-      live: project.gsx$live.$t,
-      github: project.gsx$github.$t,
+      link: project.gsx$link.$t,
     };
   });
   console.log(projects);
@@ -32,7 +31,7 @@ $.ajax(
     const $a = $("<a>");
     $a.addClass("cardLink");
     $a.attr("target", "_blank");
-    $a.attr("href", `${projects[i].github}`);
+    $a.attr("href", `${projects[i].link}`);
     $a.html(`
     <div class="card">
     <div class="cardContainer">
