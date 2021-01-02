4e5e810b59f480ee6024fcf03ae72f222c0ba4bc
Kaung Shein <nicholasling1412@gmail.com>
2021-01-02 18:00:44 -0500

    Finalized Shoot Gallery + added fade-in effects to pages

--- js/app.js
+++ js/app.js
@@ -54,9 +54,51 @@ $.ajax(
   }
 });
 
-////////////////////////////////
-// Pull data from blog from Headless CMS
-////////////////////////////////
+///////////////////////////////////
+// PULL AND RENDER DATA FROM GOOGLE SHEET (SHOOT)
+///////////////////////////////////
+$.ajax(
+  "https://spreadsheets.google.com/feeds/list/1ZYlV-gAMjH1KxIEfc24jhPFtLRe3hfE-3TcwIzMoh1o/2/public/full?alt=json"
+).then((data) => {
+  // Checking my data
+  console.log(data);
+
+  // Put our projects in a variable
+  const rawProjects = data.feed.entry;
+
+  // Log our projects
+  console.log(rawProjects);
+
+  // Prettify our projects array
+  const projects = rawProjects.map((project) => {
+    return {
+      name: project.gsx$name.$t,
+      image: project.gsx$image.$t,
+      link: project.gsx$link.$t,
+    };
+  });
+  console.log(projects);
+
+  // Use jQuery to render projects to page
+  for (i = 0; i < projects.length; i++) {
+    const $a = $("<a>");
+    $a.addClass("cardLink");
+    $a.attr("target", "_blank");
+    $a.attr("href", `${projects[i].link}`);
+    $a.html(`
+    <div class="card">
+      <img
+        src="${projects[i].image}"
+      />
+      <div class="cardOverlay">
+      <div class="cardOverlayText">
+        ${projects[i].name}
+      </div>
+    </div>
+    `);
+    $(".shootGallery").append($a);
+  }
+});
 
 ////////////////////////////
 // Overlay Mobile Navigation
